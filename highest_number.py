1294bbd30f7ac832912e48937d8571402d9734ec
ChauncyAclao <rheignchauncy@gmail.com>
2024-10-15 15:57:35 +0800

    first commit

---
+++ highest_number.py
@@ -0,0 +1,12 @@
+
+#define 5 variables 
+#compare num1 with num2-5
+    #if num1 greater print num1
+#compare num2 with num1-5 except 2
+    #if num2 greater print num2
+#compare num3 with num1-5 except 3
+    #if num3 greater print num3
+#compare num4 with num1-5 except 4
+    #if num4 greater print num4
+#compare num5 with num1-4 
+    #if num5 greater print num5
