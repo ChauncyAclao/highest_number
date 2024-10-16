c2d8d19c7cdebd9d3afbe364ceb3ef9112e89bae
ChauncyAclao <rheignchauncy@gmail.com>
2024-10-16 16:37:33 +0800

    4th commit

--- highest_number.py
+++ highest_number.py
@@ -12,19 +12,22 @@
     #if num5 greater print num5
 
 
-num1 = int(input("1st:"))
-num2 = int(input("2nd:"))
-num3 = int(input("3rd:"))
-num4 = int(input("4th:"))
-num5 = int(input("5th:"))
+def highest_number(num1, num2, num3, num4, num5):
+    if num1 >= num2 and num1 >= num3 and num1 >= num4 and num1 >= num5:
+        print("highest number:", num1) 
+    elif num2 >= num1 and num2 >= num3 and num2 >= num4 and num2>= num5:
+        print("highest number:", num2) 
+    elif num3 >= num1 and num3 >= num2 and num3 >= num4 and num3 >= num5: 
+        print("highest number:", num3)
+    elif num4 >= num1 and num4 >= num2 and num4 >= num3 and num4 >= num5:
+        print("highest number:", num4)
+    elif num5 >= num1 and num5 >= num2 and num5 >= num3 and num5 >= num4:
+        print("highest number:", num5)
 
-if num1 >= num2 and num1 >= num3 and num1 >= num4 and num1 >= num5:
-    print("highest number:", num1) 
-elif num2 >= num1 and num2 >= num3 and num2 >= num4 and num2>= num5:
-    print("highest number:", num2) 
-elif num3 >= num1 and num3 >= num2 and num3 >= num4 and num3 >= num5: 
-    print("highest number:", num3)
-elif num4 >= num1 and num4 >= num2 and num4 >= num3 and num4 >= num5:
-    print("highest number:", num4)
-elif num5 >= num1 and num5 >= num2 and num5 >= num3 and num5 >= num4:
-    print("highest number:", num5)
+highest_number(
+    num1 = int(input("1st:")),
+    num2 = int(input("2nd:")),
+    num3 = int(input("3rd:")),
+    num4 = int(input("4th:")),
+    num5 = int(input("5th:"))
+    )
