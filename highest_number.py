58ea29636d8d20a8a4693df0d8e871379ff72f39
ChauncyAclao <rheignchauncy@gmail.com>
2024-10-16 16:28:30 +0800

    3rd commit

--- highest_number.py
+++ highest_number.py
@@ -18,4 +18,13 @@ num3 = int(input("3rd:"))
 num4 = int(input("4th:"))
 num5 = int(input("5th:"))
 
-
+if num1 >= num2 and num1 >= num3 and num1 >= num4 and num1 >= num5:
+    print("highest number:", num1) 
+elif num2 >= num1 and num2 >= num3 and num2 >= num4 and num2>= num5:
+    print("highest number:", num2) 
+elif num3 >= num1 and num3 >= num2 and num3 >= num4 and num3 >= num5: 
+    print("highest number:", num3)
+elif num4 >= num1 and num4 >= num2 and num4 >= num3 and num4 >= num5:
+    print("highest number:", num4)
+elif num5 >= num1 and num5 >= num2 and num5 >= num3 and num5 >= num4:
+    print("highest number:", num5)
